dd2a47d8dac8b37f5e5b58469566910f58488b26
marcoq <marcojasso@gmail.com>
2018-05-21 19:20:16 -0500

    material-design - Material design package installation  - material design was added using schematics  - ng add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RoutingModule } from './routing/routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { RoutingModule } from './routing/routing.module';
     AuthenticationModule,
     DashboardModule,
     RoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
